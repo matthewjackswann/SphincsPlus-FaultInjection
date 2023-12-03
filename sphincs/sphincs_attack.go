d3af4f48e527fd0cafda08f75b781fa89ea80142
Matthew Swann <cv20549@bristol.ac.uk>
2023-12-03 20:21:23 +0000

    moved attack function into sphincs_attack.go

--- sphincs/sphincs_attack.go
+++ sphincs/sphincs_attack.go
@@ -1,7 +1,6 @@
 package sphincs
 
 import (
-	"crypto/rand"
 	"github.com/kasperdi/SPHINCSPLUS-golang/address"
 	"github.com/kasperdi/SPHINCSPLUS-golang/fors"
 	"github.com/kasperdi/SPHINCSPLUS-golang/hypertree"
@@ -10,23 +9,16 @@ import (
 	"math"
 )
 
-func Spx_sign_fault(params *parameters.Parameters, M []byte, SK *SPHINCS_SK) *SPHINCS_SIG {
+func Spx_verify_get_msg_sig_tree(params *parameters.Parameters, M []byte, SIG *SPHINCS_SIG, PK *SPHINCS_PK) (bool, []byte, []byte, uint64) {
 	// init
 	adrs := new(address.ADRS)
-
-	// generate randomizer
-	opt := make([]byte, params.N)
-	if params.RANDOMIZE {
-		rand.Read(opt)
-	}
-
-	R := params.Tweak.PRFmsg(SK.SKprf, opt, M)
-
-	SIG := new(SPHINCS_SIG)
-	SIG.R = R
+	R := SIG.GetR()
+	SIG_FORS := SIG.GetSIG_FORS()
+	SIG_HT := SIG.GetSIG_HT()
 
 	// compute message digest and index
-	digest := params.Tweak.Hmsg(R, SK.PKseed, SK.PKroot, M)
+	digest := params.Tweak.Hmsg(R, PK.PKseed, PK.PKroot, M)
+
 	tmp_md_bytes := int(math.Floor(float64(params.K*params.A+7) / 8))
 	tmp_idx_tree_bytes := int(math.Floor(float64(params.H-params.H/params.D+7) / 8))
 	tmp_idx_leaf_bytes := int(math.Floor(float64(params.H/params.D+7)) / 8)
@@ -38,25 +30,22 @@ func Spx_sign_fault(params *parameters.Parameters, M []byte, SK *SPHINCS_SK) *SP
 	idx_tree := uint64(util.BytesToUint64(tmp_idx_tree) & (math.MaxUint64 >> (64 - (params.H - params.H/params.D))))
 	idx_leaf := int(util.BytesToUint32(tmp_idx_leaf) & (math.MaxUint32 >> (32 - params.H/params.D)))
 
-	// FORS sign
+	// compute FORS public key
 	adrs.SetLayerAddress(0)
 	adrs.SetTreeAddress(idx_tree)
 	adrs.SetType(address.FORS_TREE)
 	adrs.SetKeyPairAddress(idx_leaf)
 
 	// This ensures that we avoid side effects modifying PK
-	SKseed := make([]byte, params.N)
-	copy(SKseed, SK.SKseed)
 	PKseed := make([]byte, params.N)
-	copy(PKseed, SK.PKseed)
+	copy(PKseed, PK.PKseed)
+	PKroot := make([]byte, params.N)
+	copy(PKroot, PK.PKroot)
 
-	SIG.SIG_FORS = fors.Fors_sign(params, tmp_md, SKseed, PKseed, adrs)
+	PK_FORS := fors.Fors_pkFromSig(params, SIG_FORS, tmp_md, PKseed, adrs)
 
-	PK_FORS := fors.Fors_pkFromSig(params, SIG.SIG_FORS, tmp_md, PKseed, adrs)
-
-	// sign FORS public key with HT
+	// verify HT signature
 	adrs.SetType(address.TREE)
-	SIG.SIG_HT = hypertree.Ht_sign_fault(params, PK_FORS, SKseed, PKseed, idx_tree, idx_leaf)
-
-	return SIG
+	success, msg, sig := hypertree.Ht_verify_get_msg_sig(params, PK_FORS, SIG_HT, PKseed, idx_tree, idx_leaf, PKroot)
+	return success, msg, sig, idx_tree
 }
